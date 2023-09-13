62cdbb43bacdb22089f0b97254118102d625e1c8
lazyanusha <anusha-shrestha@outlook.com>
2023-09-13 09:02:21 +0545

    ali ali comment gareko

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Time t = new Time(2, 0,0);
+        Time t = new Time(2, 30,3);
 
-        Time t2 = new Time(2, 30, 0);
+        Time t2 = new Time(2, 30, 30);
 
         // Time t3 = t.add(t2);
 
--- Time.java
+++ Time.java
@@ -1,18 +1,21 @@
+//create class Time
 public class Time {
     int hours;
     int minutes;
     int seconds;
 
+// create constructor
     Time(int hours, int minutes, int seconds) {
         this.hours = hours;
         this.minutes = minutes;
         this.seconds = seconds;
     }
 
+    // create add method
     public Time add(Time t) {
-        int ns = this.seconds + t.seconds;
-        int nm = this.minutes + t.minutes + ns / 60;
-        int nh = this.hours + t.hours + nm / 60;
+        int ns = this.seconds + t.seconds; // addition of two seconds
+        int nm = this.minutes + t.minutes + ns / 60; // addition of two minutes + if seconds is more than 60
+        int nh = this.hours + t.hours + nm / 60; // addition of two hours + if minutes is more than 60
 
         this.hours = nh;
         this.minutes = nm % 60;
@@ -20,39 +23,41 @@ public class Time {
         return this;
     }
 
+    // create subtract method
     public Time subtract(Time t) {
-        int ns = this.seconds - t.seconds;
-        int nm = this.minutes - t.minutes;
-        int nh = this.hours - t.hours;
+        int ns = this.seconds - t.seconds; // subtraction of seconds
+        int nm = this.minutes - t.minutes; // subtraction of minutes
+        int nh = this.hours - t.hours; // subtraction of hours
            
 
         if (ns < 0) {
-            nm = (nm * 60) + this.seconds;
-            ns = 60 + ns;
+            nm = (nm * 60) + this.seconds; // minutes bata 60 sec ghatako
+            ns = 60 + ns; // negative seconds laii add gareko 60 ma
         }
       
 
         if (nm < 0) {
-            nh = (nh * 60)  + this.minutes;
-            nm = 60 + nm;
+            nh = (nh * 60)  + this.minutes; // hour bata 60 minutes ghatako
+            nm = 60 + nm; // negative minute ma 60 add gareko
         }
   
         if (nh < 0) {
             System.out.println("Error: Negative time difference!");
-            nh = this.hours;
+            nh = this.hours;  // same as given time
             nm = this.minutes;
             ns = this.seconds;
         } 
         
         else {
-            this.hours = nh;
-            this.minutes = nm;
-            this.seconds = ns;
+            this.hours = nh; // new time
+            this.minutes = nm; // new time
+            this.seconds = ns; // new time
         }
-        System.out.println(nh);
+
         return this;
     }
 
+    // create display method
     public String display() {
         return hours + ":" + minutes + ":" + seconds;
     }
